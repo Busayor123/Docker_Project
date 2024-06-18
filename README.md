a37593f5177370ff28f41b943d2b5f6d17a8c82f
Busayor123 <131611646+Busayor123@users.noreply.github.com>
2024-06-18 09:25:55 -0400

    add new changes

--- README.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:409711c9b33e21ac9dbf124a60e3e9e3412eeb56a0d9a47e6441940568cbbd0f
-size 35
--- build_image.ps1
+++ build_image.ps1
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b04b568c37980402a95ac0e5e8fe828548994aa70c2e882fc8046a882178e2b
+oid sha256:0b4ae58b8c82bc2f648daf3a1dc3a06e6078b42f1d0c0310797b289e29d162e8
 size 567
